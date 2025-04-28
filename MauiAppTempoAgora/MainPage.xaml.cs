4993bd3f6004277693a95dc4790980bdb5a7f72f
BrunoFreitas09 <brunootaviocostadefreitas@gmail.com>
2025-04-28 11:29:30 -0300

    .

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -1,35 +1,125 @@
-﻿namespace MauiAppTempoAgora
+﻿using System.Diagnostics;
+using Android.Graphics;
+using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
+
+namespace MauiAppTempoAgora
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
-
-        private void OnCounterClicked(object sender, EventArgs e)
+        
+        private async void Button_Clicked_Previsao(object sender, EventArgs e)
         {
-            count++;
+            try
+            {
+                if (!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+                    if (t != null)
+                    {
+                        string dados_previsao = "";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                        dados_previsao = $"Latitude: {t.lat} \n" +
+                                         $"Longitude: {t.lon} \n" +
+                                         $"Nascer do sol: {t.sunrise} \n" +
+                                         $"Por do sol: {t.sunset} \n" +
+                                         $"Temp max: {t.temp_max} \n" +
+                                         $"Temp min: {t.temp_min} \n";
+
+                        lbl_res.Text = dados_previsao;
+
+                        string mapa = $"https://embed.windy.com/embed.html?" +
+                                      $"type=map&location=coordinates&metricRain=mm&metricTemp=°C" +
+                                      $"&metricWind=km/h&zoom=5&overlay=wind&product=ecmwf&level=surface" +
+                                      $"&lat={t.lat.ToString().Replace(",", ".")}&lon=" +
+                                      $"{t.lon.ToString().Replace(",", ".")}";
+
+                        wv_mapa.Source = mapa;
+
+                        Debug.WriteLine(mapa);
+                    }
+                    else
+                    {
+                        lbl_res.Text = "Sem dados de previsão";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
         }
 
-        private void Button_Clicked(object sender, EventArgs e)
+        private async void Button_Clicked_Localizacao(object sender, EventArgs e)
         {
+            try
+            {
+                GeolocationRequest request = new GeolocationRequest(
+                    GeolocationAccuracy.Medium,
+                    TimeSpan.FromSeconds(10)
+                );
+
+                Location? local = await Geolocation.Default.GetLocationAsync(request);
+
+                if (local != null)
+                {
+                    string local_disp = $"Latitude: {local.Latitude}\n" +
+                                        $"Longitude: {local.Longitude}";
+
+                    lbl_coords.Text = local_disp;
+
+                    //pega o nome da cidade que está nas coordenadas 
+                    GetCidade(local.Latitude, local.Longitude);
+
+
+                } else
+                {
+                    lbl_coords.Text = "Nenhuma localização";
+                }
+            }
+
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                await DisplayAlert("Erro: Dispositivo não Suporta", fnsEx.Message, "OK");
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                await DisplayAlert("Erro: Localização Desabilitada", fneEx.Message, "OK");
+            }
+            catch (PermissionException pEx)
+            {
+                await DisplayAlert("Erro: Permissão da Localização", pEx.Message, "OK");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro", ex.Message, "OK");
+            }
+
 
         }
 
-        private void Button_Clicked_1(object sender, EventArgs e)
+        private async void GetCidade(double lat, double lon)
         {
+            try
+            {
+                IEnumerable<Placemark > places = await Geocoding.Default.GetPlacemarksAsync(
+                    lat, lon);
 
+                Placemark? place = places.FirstOrDefault();
+
+                if(place != null)
+                {
+                    txt_cidade.Text = place.Locality;
+                }
+            } catch (Exception ex)
+            {
+                await DisplayAlert("Erro: obtenção do nome da cidade",ex.Message.OK);
+            }
         }
     }
-
 }
